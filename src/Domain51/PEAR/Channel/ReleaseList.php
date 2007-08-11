48a368bcf3c993e366d96f4ef6cd60304944535f
tswicegood <tswicegood@159b4bd8-5ae1-4aa7-97ff-061b2ff7a14b>
2007-08-11 03:46:47 +0000

    Add Iterator capability to ReleaseList
    git-svn-id: svn://localhost/Domain51_PEAR_Channel_Frontend/trunk@260 159b4bd8-5ae1-4aa7-97ff-061b2ff7a14b

--- src/Domain51/PEAR/Channel/ReleaseList.php
+++ src/Domain51/PEAR/Channel/ReleaseList.php
@@ -1,6 +1,6 @@
 <?php
 
-class Domain51_PEAR_Channel_ReleaseList
+class Domain51_PEAR_Channel_ReleaseList implements Iterator
 {
     private $_config = null;
     private $_raw_data = array();
@@ -15,11 +15,42 @@ class Domain51_PEAR_Channel_ReleaseList
             ':channel' => (string)$package->channel,
         ));
         $this->_raw_data = $this->_data = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $this->_config = $config;
     }
     
     public function count()
     {
         return count($this->_data);
+    }
+    
+    public function current()
+    {
+        $data = current($this->_data);
+        $data['_RAW_VALUES'] = true;
+        return new Domain51_PEAR_Channel_Release(
+            $this->_config,
+            $data
+        );
+    }
+    
+    public function key()
+    {
+        return key($this->_data);
+    }
+    
+    public function next()
+    {
+        next($this->_data);
+    }
+    
+    public function rewind()
+    {
+        reset($this->_data);
+    }
+    
+    public function valid()
+    {
+        return current($this->_data) !== false;
     }
     
     public function filter($type)
