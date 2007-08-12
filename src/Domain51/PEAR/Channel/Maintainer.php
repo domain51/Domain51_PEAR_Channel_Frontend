6391c5f2df39778eb41d6381563fb415243d10a9
tswicegood <tswicegood@159b4bd8-5ae1-4aa7-97ff-061b2ff7a14b>
2007-08-12 20:38:20 +0000

    Add in basic Maintainer model
    git-svn-id: svn://localhost/Domain51_PEAR_Channel_Frontend/trunk@272 159b4bd8-5ae1-4aa7-97ff-061b2ff7a14b

---
+++ src/Domain51/PEAR/Channel/Maintainer.php
@@ -0,0 +1,26 @@
+<?php
+
+class Domain51_PEAR_Channel_Maintainer extends Domain51_PEAR_Channel_AbstractDBModel
+{
+    public function __construct(Domain51_PEAR_Channel_Config $config, $criteria)
+    {
+        parent::__construct($config, $criteria);
+    }
+    
+    public function __get($key)
+    {
+        switch ($key) {
+            case 'package' :
+                return new Domain51_PEAR_Channel_Package(
+                    $this->_config,
+                    array(
+                        'package' => $this->_data['package'],
+                        'channel' => $this->_data['channel'],
+                    )
+                );
+            
+            default :
+                return parent::__get($key);
+        }
+    }
+}
