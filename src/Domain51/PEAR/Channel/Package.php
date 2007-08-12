4764e08079236c798da53f976ffeb2f223de95f7
tswicegood <tswicegood@159b4bd8-5ae1-4aa7-97ff-061b2ff7a14b>
2007-08-12 04:20:48 +0000

    Add ability for Packages to control children Add ability for PackageLists to have criteria (first pass)
    git-svn-id: svn://localhost/Domain51_PEAR_Channel_Frontend/trunk@265 159b4bd8-5ae1-4aa7-97ff-061b2ff7a14b

--- src/Domain51/PEAR/Channel/Package.php
+++ src/Domain51/PEAR/Channel/Package.php
@@ -58,6 +58,19 @@ class Domain51_PEAR_Channel_Package
             case 'releases' :
                 return new Domain51_PEAR_Channel_ReleaseList($this->_config, $this);
             
+            case 'has_children' :
+                $statement = $this->_config->pdo->prepare("SELECT COUNT(*) FROM packages WHERE parent = :package");
+                $statement->execute(array(
+                    ':package' => (string)$this->package,
+                ));
+                return $statement->fetchColumn() > 0;
+            
+            case 'childPackages' :
+                return new Domain51_PEAR_Channel_PackageList(
+                    $this->_config,
+                    array('parent' => (string)$this->package)
+                );
+            
             default:
                 return $this->_data[$key];
         }
--- src/Domain51/PEAR/Channel/PackageList.php
+++ src/Domain51/PEAR/Channel/PackageList.php
@@ -5,12 +5,21 @@ class Domain51_PEAR_Channel_PackageList implements Iterator
     private $_config = null;
     private $_data = null;
     
-    public function __construct(Domain51_PEAR_Channel_Config $config, $criteria = null)
+    public function __construct(Domain51_PEAR_Channel_Config $config, array $criteria = null)
     {
         $this->_config = $config;
         $where_sql = '';
+        $real_criteria = array();
+        if (is_array($criteria) && count($criteria) > 0) {
+            $temp_criteria = array();
+            foreach ($criteria as $key => $value) {
+                $temp_criteria[] = "{$key} = :{$key}";
+                $real_criteria[":{$key}"] = $value;
+            }
+            $where_sql = "WHERE " . implode(' AND ', $temp_criteria);
+        }
         $statement = $config->pdo->prepare("SELECT * FROM packages {$where_sql}");
-        $statement->execute();
+        $statement->execute($real_criteria);
         $this->_data = $statement->fetchAll(PDO::FETCH_ASSOC);
     }
     
