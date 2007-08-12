c997bfc1136a1d97ea143c2ca504bcff9a0a778c
tswicegood <tswicegood@159b4bd8-5ae1-4aa7-97ff-061b2ff7a14b>
2007-08-12 04:28:46 +0000

    Refactor common iterator functionality into abstract class
    git-svn-id: svn://localhost/Domain51_PEAR_Channel_Frontend/trunk@266 159b4bd8-5ae1-4aa7-97ff-061b2ff7a14b

--- src/Domain51/PEAR/Channel/PackageList.php
+++ src/Domain51/PEAR/Channel/PackageList.php
@@ -1,12 +1,12 @@
 <?php
 
-class Domain51_PEAR_Channel_PackageList implements Iterator
+class Domain51_PEAR_Channel_PackageList extends Domain51_PEAR_Channel_AbstractIterator
 {
-    private $_config = null;
-    private $_data = null;
-    
+    protected $_type = 'Package';
     public function __construct(Domain51_PEAR_Channel_Config $config, array $criteria = null)
     {
+        parent::__construct();
+        
         $this->_config = $config;
         $where_sql = '';
         $real_criteria = array();
@@ -21,37 +21,5 @@ class Domain51_PEAR_Channel_PackageList implements Iterator
         $statement = $config->pdo->prepare("SELECT * FROM packages {$where_sql}");
         $statement->execute($real_criteria);
         $this->_data = $statement->fetchAll(PDO::FETCH_ASSOC);
-    }
-    
-    public function count()
-    {
-        return count($this->_data);
-    }
-    
-    public function current()
-    {
-        $data = current($this->_data);
-        $data['_RAW_VALUES'] = true;
-        return new Domain51_PEAR_Channel_Package($this->_config, $data);
-    }
-    
-    public function key()
-    {
-        return key($this->_data);
-    }
-    
-    public function next()
-    {
-        next($this->_data);
-    }
-    
-    public function rewind()
-    {
-        reset($this->_data);
-    }
-    
-    public function valid()
-    {
-        return current($this->_data) !== false;
     }
 }
--- src/Domain51/PEAR/Channel/ReleaseList.php
+++ src/Domain51/PEAR/Channel/ReleaseList.php
@@ -1,13 +1,14 @@
 <?php
 
-class Domain51_PEAR_Channel_ReleaseList implements Iterator
+class Domain51_PEAR_Channel_ReleaseList extends Domain51_PEAR_Channel_AbstractIterator
 {
-    private $_config = null;
     private $_raw_data = array();
-    private $_data = array();
+    protected $_type = 'Release';
     
     public function __construct(Domain51_PEAR_Channel_Config $config, Domain51_PEAR_Channel_Package $package)
     {
+        parent::__construct();
+        
         $query = "SELECT * FROM releases WHERE package = :package AND channel = :channel";
         $statement = $config->pdo->prepare($query);
         $statement->execute(array(
@@ -16,41 +17,6 @@ class Domain51_PEAR_Channel_ReleaseList implements Iterator
         ));
         $this->_raw_data = $this->_data = $statement->fetchAll(PDO::FETCH_ASSOC);
         $this->_config = $config;
-    }
-    
-    public function count()
-    {
-        return count($this->_data);
-    }
-    
-    public function current()
-    {
-        $data = current($this->_data);
-        $data['_RAW_VALUES'] = true;
-        return new Domain51_PEAR_Channel_Release(
-            $this->_config,
-            $data
-        );
-    }
-    
-    public function key()
-    {
-        return key($this->_data);
-    }
-    
-    public function next()
-    {
-        next($this->_data);
-    }
-    
-    public function rewind()
-    {
-        reset($this->_data);
-    }
-    
-    public function valid()
-    {
-        return current($this->_data) !== false;
     }
     
     public function filter($type)
