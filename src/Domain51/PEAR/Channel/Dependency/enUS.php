c9fe4dae7829bd3346449b55098779927d3ef0be
tswicegood <tswicegood@159b4bd8-5ae1-4aa7-97ff-061b2ff7a14b>
2007-08-19 16:53:01 +0000

    Add in the rest of the known package types
    git-svn-id: svn://localhost/Domain51_PEAR_Channel_Frontend/trunk@295 159b4bd8-5ae1-4aa7-97ff-061b2ff7a14b

--- src/Domain51/PEAR/Channel/Dependency/enUS.php
+++ src/Domain51/PEAR/Channel/Dependency/enUS.php
@@ -6,6 +6,12 @@ class Domain51_PEAR_Channel_Dependency_enUS extends Domain51_PEAR_Channel_Depend
         'pkg' => 'Package',
         'ext' => 'PHP Extension',
         'php' => 'PHP',
+        'prog' => 'Program',
+        'ldlib' => 'Development Library',
+        'rtlib' => 'Runtime Library',
+        'os' => 'Operating System',
+        'websrv' => 'Web Server',
+        'sapi' => 'SAPI Backend',
     );
     
     protected $_translated_version = array(
