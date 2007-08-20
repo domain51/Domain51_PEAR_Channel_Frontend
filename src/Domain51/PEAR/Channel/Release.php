58099e3c456ef6ff9e5128ec43301546dd1abe8c
tswicegood <tswicegood@159b4bd8-5ae1-4aa7-97ff-061b2ff7a14b>
2007-08-20 00:53:20 +0000

    Add dependencies property to Release
    git-svn-id: svn://localhost/Domain51_PEAR_Channel_Frontend/trunk@298 159b4bd8-5ae1-4aa7-97ff-061b2ff7a14b

--- src/Domain51/PEAR/Channel/Release.php
+++ src/Domain51/PEAR/Channel/Release.php
@@ -8,6 +8,21 @@ class Domain51_PEAR_Channel_Release extends Domain51_PEAR_Channel_AbstractDBMode
             $criteria = array('id' => $criteria);
         }
         parent::__construct($config, $criteria);
+        
+        $this->_initDependencies();
+    }
+    
+    private function _initDependencies()
+    {
+        $this->_data['dependencies'] = array();
+        if (($deps = unserialize($this->deps)) === false) {
+            return;
+        }
+        
+        $dependency_object = 'Domain51_PEAR_Channel_Dependency_' . $this->_config->locale;
+        foreach ($deps as $dep_data) {
+            $this->_data['dependencies'][] = new $dependency_object($this->_config, $dep_data);
+        }
     }
 }
 
